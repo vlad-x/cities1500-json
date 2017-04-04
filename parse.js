b09bcf43a16359299efee952d5a165bdad9fa91e
Vlad <vlad@difogic.com>
2017-04-04 14:22:16 +0300

    Initial import

---
+++ parse.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+
+const data = fs.readFileSync('cities15000.txt').toString()
+
+const fields = [
+  'id',
+  'name',
+  'asciiname',
+  'alternativeNames',
+  'lat',
+  'lon',
+  'featureClass',
+  'featureCode',
+  'country',
+  'altCountry',
+  'adminCode',
+  'countrySubdivision',
+  'municipality',
+  'municipalitySubdivision',
+  'population',
+  'elevation',
+  'dem',
+  'tz',
+  'lastModified'
+]
+
+const out = data.split('\n')
+  .map(c => {
+    const res = c.split(/\t/g)
+
+    const obj = fields.reduce((obj, key, idx) => Object.assign(obj, {[key]: res[idx] }), {})
+    obj.alternativeNames = (obj.alternativeNames || '').split(',')
+
+    console.log(obj.id, obj.name, obj.population)
+    return obj
+  })
+
+fs.writeFileSync('cities.json', JSON.stringify(out, false, 2))
